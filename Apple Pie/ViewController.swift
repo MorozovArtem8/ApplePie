f7d69c9f62d242ef68c539248b356e390bc50666
MorozovArtem8 <Vjhjpjaa32@yandex.ru>
2023-12-31 14:37:22 +0300

    first

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+class ViewController: UIViewController {
+    var currentGame: Game!
+    var listOfWirds = ["buccaneer", "swift", "glorious",
+                       "incandescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0 {
+        didSet{
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet{
+            newRound()
+        }
+    }
+    
+    // MARK: IBOutlet
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    // MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+    
+    func newRound () {
+        let newWrod = listOfWirds.removeFirst()
+        currentGame = Game(word: newWrod, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+        updateUI()
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        }else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        }else{
+            updateUI()
+        }
+    }
+    
+    // MARK: @IBAction
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.configuration!.title!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+
+}
+
